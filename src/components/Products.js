1ec46be0e964e0fb9c306d504ba67bacbfde1a22
Dinesh <dineshyalla@gmail.com>
2019-11-03 17:54:24 -0800

    Basic Products List

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 function Products(){
+
+    const [ Products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const productList = [
+            {name:'book', id:1}, {name:'laptop', id:2}, {name:'game console', id:3}, {name:'radio', id:4}, {name:'notenook', id:56}
+        ]
+        setProducts(productList);
+    }, [])
+
+    const prodlist = Products.map((i,index) => {
+        return <li key={index}>id:{i.id} | product name: <strong>{i.name}</strong>  <Addbutton pd={i}/></li>
+      })
+
     return ( 
-        <div>
-            <h2> PRODUCTS </h2>
-        </div>);
+        <ul>
+            {prodlist}
+        </ul>);
 };
+
+function Addbutton(props){
+
+    return(
+            <button>
+              add to cart
+            </button>    
+    )
+  }
+
 
 export default Products;
